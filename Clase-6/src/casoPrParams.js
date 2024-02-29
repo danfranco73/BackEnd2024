5492af25a8068abba4e39e86f8763d1c016d33b4
danfranco73 <104471109+danfranco73@users.noreply.github.com>
2024-02-29 19:32:41 -0300

    Clase6

--- Clase-6/src/casoPrParams.js
+++ Clase-6/src/casoPrParams.js
@@ -2,3 +2,29 @@
 La ruta raíz ‘/’ debe devolver todos los usuarios
 la ruta /:userId debe devolver sólo al usuario con dicho Id.
  */
+
+import express from 'express';
+
+const app = express();  // Crear un servidor express
+
+const usuarios = [
+    { id: 1, nombre: 'William' },
+    { id: 2, nombre: 'William' },
+    { id: 3, nombre: 'William' },
+    { id: 4, nombre: 'William' },
+];
+
+app.get('/', (req, res) => {
+    res.json(usuarios);
+});
+
+app.get('/:userId', (req, res) => {
+    const { userId } = req.params;
+    const usuario = usuarios.find(usuario => usuario.id == userId);
+    res.json(usuario);
+});
+
+app.listen(8080, () => {
+    console.log('Servidor express escuchando en el puerto 8080');
+});
+
