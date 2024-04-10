b24945eba15052bd78deddbfbe10bc88c8982cb8
danfranco73 <104471109+danfranco73@users.noreply.github.com>
2024-04-10 19:37:34 -0300

    p y c ok

--- Clase15-PracticaIntegradora/public/js/index.js
+++ Clase15-PracticaIntegradora/public/js/index.js
@@ -1,27 +1,47 @@
+// en funcion a mi home.handlebars armare el index.js que solo muestra los productos
 
-const socket = io.connect();
+const socket = io();
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+let data = [];
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (input.value.trim() === "") {
-    return;
-  }
-  socket.emit("newProduct", input.value);
-  input.value = "";
-} );
+socket.emit("getProducts");
 
-socket.on("sendProducts", (data) => {
-  ul.innerHTML = "";
-  data.forEach((product) => {
+socket.on("sendProducts", (products) => {
+  data = products;
+  updateList(data);
+}
+);
+
+socket.on("getProducts", (products) => {
+  data = products;
+  updateList(data);
+}
+);
+
+socket.on("updateProducts", (products) => {
+  data = products;
+  updateList(data);
+}
+);
+
+const list = document.getElementById("list");
+
+const updateList = async (data) => {
+  list.innerHTML = "";
+  await data.forEach((product) => {
     const li = document.createElement("li");
-    li.textContent = product;
-    ul.appendChild(li);
+    li.textContent = `Title: ${product.title} - Price: ${product.price}`;
+    list.appendChild(li);
   });
-} );
+}
+
+socket.on("connect", () => {
+  console.log("Connected!");
+});
+
+socket.on("disconnect", () => {
+  console.log("Disconnected!");
+});
 
 
 
